ba0ab2a06e4722274f07627498a36b5ac7b0dcca
wuyu0920 <823986830@qq.com>
2017-03-01 17:02:20 +0800

    提交

---
+++ js/demo.js
@@ -0,0 +1,36 @@
+﻿//初始化配置
+   var initDialog={
+        option:{},
+        url:{
+          alertUrl: "alert.htm",
+          msgUrl:"msg.htm",
+        	confirmUrl:"confirm.htm"
+        }
+
+   };
+
+
+
+/*qhwDialog.alert(type,msg,ok);
+qhwDialog.popMsg(type,msg);
+qhwDialog.confirm(msg,ok,cancle);
+qhwDialog.open(url,callback);*/
+
+/*qhwDialog.alert('s',hello world!,huidiao);
+function huidiao(){
+	alert("你点击确定了");
+}*/
+//qhwDialog.popMsg('w',"hello world!");
+/*qhwDialog.confirm("hello world!",queding,quxiao);
+function queding(){
+	alert("你点击确定了");
+}
+function quxiao(){
+	alert("你点击取消");
+}*/
+
+/*qhwDialog.loading();*/
+
+
+
+
